fa4c281d5b25e98b647c35481dfe889614032a00
TeoRicciuti <teoricciuti5@gmail.com>
2025-10-26 21:46:07 -0300

    Seccion Registrar libros terminada

--- TP-Grupal/Grupo N° 9/trabajoPracticoCuatro/src/App.jsx
+++ TP-Grupal/Grupo N° 9/trabajoPracticoCuatro/src/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Inicio from "./Containers/Inicio"
+import RegistrarLibro from "./Containers/RegistrarLibro"
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/inicio" element={<Inicio/>}/>
+        <Route path="/registrarLibro" element={<RegistrarLibro/>}/>
       </Routes>
     </Router>
   )
